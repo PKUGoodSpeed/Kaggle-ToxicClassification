bee5de8d75a23567eeaa785ac2178f6df277e611
Zebo Li <zebo@ginkgo-llc.com>
2018-01-29 11:19:01 -0800

    sdf

--- pkugoodspeed/src/run.sh
+++ pkugoodspeed/src/run.sh
@@ -1,2 +1,3 @@
+python single_label_run.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/cnn.cfg 
 python single_label_run.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/rnn.cfg 
 python single_label_run.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/rcnn.cfg 
