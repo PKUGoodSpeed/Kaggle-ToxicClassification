dc97090b8d1d3bea456b5b132ed0771c0d8050e0
Zebo Li <zebo@ginkgo-llc.com>
2018-01-31 16:26:41 -0800

    sdf

---
+++ pkugoodspeed/src/run.sh
@@ -0,0 +1,6 @@
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_cnnx2_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_cnnx3_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_grux1_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_grux2_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_lstmx1_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_lstmx2_emb.cfg
