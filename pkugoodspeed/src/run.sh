011497cc1f76370c1bdb1fc326337502fffb092f
Zebo Li <zebo@ginkgo-llc.com>
2018-02-08 23:43:51 -0800

    modify run.sh

--- pkugoodspeed/src/run.sh
+++ pkugoodspeed/src/run.sh
@@ -1,6 +1,2 @@
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_cnnx2_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_cnnx3_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_grux1_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_grux2_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_lstmx1_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_lstmx2_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_cnnx1_rnnx1_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_cnnx1_rnnx2_emb.cfg
