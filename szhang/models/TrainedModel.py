841cb2978ee76fba753ba2b7a40ac5d81de3d8ca
Sai Zhang <saizhang@Sais-MacBook-Pro-2.local>
2018-02-12 16:13:48 -0800

    added trained model class to persist model

--- szhang/models/TrainedModel.py
+++ szhang/models/TrainedModel.py
@@ -1,3 +1,6 @@
+import os,sys
+sys.path.append('..')
+import modelDB
 
 class TrainedModel(object):
     ''' each trained model needs to implement predict method
@@ -9,7 +12,41 @@ class TrainedModel(object):
     def predict(self, test, **kwargs):
         raise ValueError("Needs to be implemented")
 
-    def save(self, mDB, nameKey):
+    def save(self, mDB, nameKey, modelpath, **kwargs):
+        ''' save model into modelpath/nameKey,
+            update the information into modelDB
+            :mDB: meta data frame storing all models info
+            :nameKey: unique identifier for each saved model
+            :modelpath: subdir inside modelDB dir, e.g. if modelDB is /root/modelDB, then modelpath is /cnn
+        '''
+
+        #house keeping
+        absPath = os.path.join(modelDB.MODEL_DB_ROOT, modelpath)
+        if not os.path.exists( absPath ):
+            print("Creating model path {}".format(absPath))
+            os.mkdir(absPath)
+
+        #check for nameKey conflict
+        #schema of db
+        # 'modelName', type {rnn, cnn, rf}, date, model
+        if (not mDB.empty) and any( mDB['modelName'] == nameKey ):
+            delete = raw_input("Warning: found identical nameKey, overwrite: [y/n]?")
+            if delete != 'y' and delete != 'Y':
+                print("aborting")
+                return
+            else:
+                mDB = mDB[mDB['modelName'] != nameKey]
+
+        mDB = self._save(mDB, nameKey, modelpath, **kwargs)
+        return mDB
+
+    def _save(self, mDB, nameKey, modelpath, **kwargs):
+        '''
+            to be implemented by subclass
+        '''
+        raise ValueError("Needs to be implemented")
+
+    def load(self, mDB, nameKey, modelpath):
         raise ValueError("Needs to be implemented")
 
 
