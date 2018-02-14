27b8b14c4c91b78d8f7fcb09b3acb0d38397008d
Zebo Li <zebo@ginkgo-llc.com>
2018-02-13 21:19:15 -0800

    sdf

--- pkugoodspeed/src/run.sh
+++ pkugoodspeed/src/run.sh
@@ -1,4 +1,4 @@
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_bgrux1_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_bgrux2_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_blstmx1_emb.cfg
-python main.py --train ../data/train_processed.csv --test ../data/train_processed.csv --config cfgs/seq_blstmx2_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/seq_bgrux1_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/seq_bgrux2_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/seq_blstmx1_emb.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/seq_blstmx2_emb.cfg
