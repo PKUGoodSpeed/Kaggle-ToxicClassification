612594f362b83e8bd05f982ba53106f0bcd18123
Zebo Li <zebo@ginkgo-llc.com>
2018-02-22 18:01:42 -0800

    sdf

--- pkugoodspeed/src/run.sh
+++ pkugoodspeed/src/run.sh
@@ -1,7 +1,4 @@
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/toxic01.cfg
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/toxic02.cfg
 python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/toxic03.cfg
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/toxic04.cfg
 python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/toxic05.cfg
 python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/toxic06.cfg
 python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/toxic07.cfg
