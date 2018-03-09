c1235d9a4f4d612eb7b2ee0a9a5a7383f0ca8fbc
Zebo Li <zebo@ginkgo-llc.com>
2018-03-08 21:08:28 -0800

    sdf

--- pkugoodspeed/src/run.sh
+++ pkugoodspeed/src/run.sh
@@ -1,2 +1,6 @@
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/all/config01.cfg
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/all/config02.cfg
+python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_glove.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_glove.cfg
+python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_lexW.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_lexW.cfg
+python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_ft.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_ft.cfg
