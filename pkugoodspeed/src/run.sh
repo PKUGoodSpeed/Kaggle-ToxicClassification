0117aa404e5c9f306e2a35ee8e6751fbfd9414fe
Zebo Li <zebo@ginkgo-llc.com>
2018-03-08 21:27:33 -0800

    sdf

--- pkugoodspeed/src/run.sh
+++ pkugoodspeed/src/run.sh
@@ -1,6 +1,6 @@
-python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_glove.cfg
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_glove.cfg
-python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_lexW.cfg
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_lexW.cfg
-python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_ft.cfg
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/sever_ft.cfg
+python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_glove.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_glove.cfg
+python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_lexW.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_lexW.cfg
+python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_ft.cfg
+python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_ft.cfg
