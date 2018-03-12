173fda7a32d2cdcd78cbb39f0f6cd265b54a7f36
Zebo Li <zebo@ginkgo-llc.com>
2018-03-12 09:05:37 -0700

    sdf

--- pkugoodspeed/src/run.sh
+++ pkugoodspeed/src/run.sh
@@ -2,5 +2,3 @@ python fold.py --train ../data/train_processed.csv --test ../data/test_processed
 python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_glove.cfg
 python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_lexW.cfg
 python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_lexW.cfg
-python fold.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_ft.cfg
-python main.py --train ../data/train_processed.csv --test ../data/test_processed.csv --config cfgs/obscene_ft.cfg
